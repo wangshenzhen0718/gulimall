039a5fef27733d2fec51b75e497cda724d8ba682
wangshenzhen0718 <153474100@qq.com>
2022-09-21 23:24:58 +0800

    分组校验

---
+++ gulimall-common/src/main/java/com/wang/common/valid/AddGroup.java
@@ -0,0 +1,4 @@
+package com.wang.common.valid;
+
+public interface AddGroup {
+}
--- gulimall-product/src/main/java/com/wang/gulimall/product/entity/BrandEntity.java
+++ gulimall-product/src/main/java/com/wang/gulimall/product/entity/BrandEntity.java
@@ -5,6 +5,9 @@ import com.baomidou.mybatisplus.annotation.TableName;
 
 import java.io.Serializable;
 import java.util.Date;
+
+import com.wang.common.valid.AddGroup;
+import com.wang.common.valid.UpdateGroup;
 import lombok.Data;
 import org.hibernate.validator.constraints.URL;
 
@@ -26,17 +29,21 @@ public class BrandEntity implements Serializable {
 	 * 品牌id
 	 */
 	@TableId
+	@Null(groups = AddGroup.class,message = "新增不能指定ID")
+	@NotNull(groups = UpdateGroup.class,message = "修改必须指定品牌ID")
 	private Long brandId;
 	/**
 	 * 品牌名
 	 */
 	@NotBlank(message = "名字不能为空！！！")
+	@NotBlank(groups = {AddGroup.class,UpdateGroup.class},message = "品牌名必须提交")
 	private String name;
 	/**
 	 * 品牌logo地址
 	 */
 	@NotEmpty
 	@URL(message = "logo必须是合法的url地址")
+	@NotBlank(groups = AddGroup.class)
 	private String logo;
 	/**
 	 * 介绍
@@ -45,17 +52,18 @@ public class BrandEntity implements Serializable {
 	/**
 	 * 显示状态[0-不显示；1-显示]
 	 */
+	@NotNull(groups = {AddGroup.class,UpdateGroup.class})
 	private Integer showStatus;
 	/**
 	 * 检索首字母
 	 */
-	@NotEmpty
+	@NotEmpty(groups = {AddGroup.class})
 	@Pattern(regexp = "/^[a-zA-Z]$/",message = "第一个字母必须是一个字符")
 	private String firstLetter;
 	/**
 	 * 排序
 	 */
-	@NotNull
+	@NotNull(groups = AddGroup.class)
 	@Min(value = 0,message = "排序必须大于等于0")
 	private Integer sort;
 
