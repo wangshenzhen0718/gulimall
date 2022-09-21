282a681cc082f792236d7523536213a9f425404e
wangshenzhen0718 <153474100@qq.com>
2022-09-21 22:42:12 +0800

    format

--- gulimall-product/src/main/java/com/wang/gulimall/product/exception/MallExceptionControllerAdvoce.java
+++ gulimall-product/src/main/java/com/wang/gulimall/product/exception/MallExceptionControllerAdvoce.java
@@ -5,28 +5,29 @@ import com.wang.common.utils.R;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@ResponseBody
+//@ResponseBody
+//@RestControllerAdvice(value = "com.wang.gulimall.product.controller")
 @RestControllerAdvice(value = "com.wang.gulimall.product.controller")
 public class MallExceptionControllerAdvoce {
-    @ExceptionHandler(value= MethodArgumentNotValidException.class)
-    public R handleValidException(MethodArgumentNotValidException e){
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public R handleValidException(MethodArgumentNotValidException e) {
         BindingResult bindingResult = e.getBindingResult();
-        Map<String,String> errorMap = new HashMap<>();
-        bindingResult.getFieldErrors().forEach((fieldError)->{
-            errorMap.put(fieldError.getField(),fieldError.getDefaultMessage());
+        Map<String, String> errorMap = new HashMap<>();
+        bindingResult.getFieldErrors().forEach((fieldError) -> {
+            errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
         });
-        return R.error(BizCodeEnum.VALID_EXCEPTION.getCode(),BizCodeEnum.VALID_EXCEPTION.getMsg()).put("data",errorMap);
+        return R.error(BizCodeEnum.VALID_EXCEPTION.getCode(), BizCodeEnum.VALID_EXCEPTION.getMsg()).put("data", errorMap);
     }
+
     @ExceptionHandler(value = Throwable.class)
-    public R handleException(Throwable throwable){
+    public R handleException(Throwable throwable) {
         System.out.println(throwable);
-        return R.error(BizCodeEnum.UNKNOWN_EXCEPTION.getCode(),BizCodeEnum.UNKNOWN_EXCEPTION.getMsg());
+        return R.error(BizCodeEnum.UNKNOWN_EXCEPTION.getCode(), BizCodeEnum.UNKNOWN_EXCEPTION.getMsg());
 
     }
 
