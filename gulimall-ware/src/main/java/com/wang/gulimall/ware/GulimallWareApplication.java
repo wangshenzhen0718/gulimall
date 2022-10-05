25e707c807f107c861629eb400f8457fe96f45f3
wangshenzhen0718 <153474100@qq.com>
2022-10-05 12:27:47 +0800

    仓库管理

--- gulimall-ware/src/main/java/com/wang/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/wang/gulimall/ware/GulimallWareApplication.java
@@ -1,10 +1,14 @@
 package com.wang.gulimall.ware;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableDiscoveryClient
+@MapperScan("com.wang.gulimall.ware.dao")
+@EnableTransactionManagement
 @SpringBootApplication
 public class GulimallWareApplication {
 
--- gulimall-ware/src/main/java/com/wang/gulimall/ware/service/impl/WareSkuServiceImpl.java
+++ gulimall-ware/src/main/java/com/wang/gulimall/ware/service/impl/WareSkuServiceImpl.java
@@ -1,16 +1,17 @@
 package com.wang.gulimall.ware.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
+import com.alibaba.nacos.client.utils.StringUtils;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.wang.common.utils.PageUtils;
 import com.wang.common.utils.Query;
-
 import com.wang.gulimall.ware.dao.WareSkuDao;
 import com.wang.gulimall.ware.entity.WareSkuEntity;
 import com.wang.gulimall.ware.service.WareSkuService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("wareSkuService")
@@ -18,12 +19,24 @@ public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> i
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<WareSkuEntity> queryWrapper = new QueryWrapper<>();
+
+        String skuId = (String) params.get("skuId");
+        if (!StringUtils.isEmpty(skuId) && !"0".equalsIgnoreCase(skuId)) {
+            queryWrapper.eq("sku_id", skuId);
+        }
+
+        String wareId = (String) params.get("wareId");
+        if (!StringUtils.isEmpty(wareId) && !"0".equalsIgnoreCase(wareId)) {
+            queryWrapper.eq("ware_id", wareId);
+        }
+
         IPage<WareSkuEntity> page = this.page(
                 new Query<WareSkuEntity>().getPage(params),
-                new QueryWrapper<WareSkuEntity>()
+                queryWrapper
         );
-
         return new PageUtils(page);
+
     }
 
 }
