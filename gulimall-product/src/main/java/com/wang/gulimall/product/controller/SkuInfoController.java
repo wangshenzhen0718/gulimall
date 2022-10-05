25a5966c0f3a1205a57f81478a69b0b14210a6fe
wangshenzhen0718 <153474100@qq.com>
2022-10-05 11:15:03 +0800

    spu sku条件查询

--- gulimall-product/src/main/java/com/wang/gulimall/product/controller/SkuInfoController.java
+++ gulimall-product/src/main/java/com/wang/gulimall/product/controller/SkuInfoController.java
@@ -1,20 +1,14 @@
 package com.wang.gulimall.product.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.wang.gulimall.product.entity.SkuInfoEntity;
-import com.wang.gulimall.product.service.SkuInfoService;
 import com.wang.common.utils.PageUtils;
 import com.wang.common.utils.R;
+import com.wang.gulimall.product.entity.SkuInfoEntity;
+import com.wang.gulimall.product.service.SkuInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
 
 
 
@@ -37,7 +31,7 @@ public class SkuInfoController {
     @RequestMapping("/list")
     //@RequiresPermissions("product:skuinfo:list")
     public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = skuInfoService.queryPage(params);
+        PageUtils page = skuInfoService.queryPageByCondition(params);
 
         return R.ok().put("page", page);
     }
--- gulimall-product/src/main/java/com/wang/gulimall/product/service/SpuInfoService.java
+++ gulimall-product/src/main/java/com/wang/gulimall/product/service/SpuInfoService.java
@@ -19,5 +19,7 @@ public interface SpuInfoService extends IService<SpuInfoEntity> {
     PageUtils queryPage(Map<String, Object> params);
 
     void savaSpuInfo(SpuSaveVo vo);
+
+    PageUtils queryPageByCondition(Map<String, Object> params);
 }
 
